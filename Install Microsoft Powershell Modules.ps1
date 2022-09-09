afd8939b3455968ca852a9c91ed18abe81f1897a
Whit3Wizard <112817193+Whit3Wizard@users.noreply.github.com>
2022-09-09 05:33:33 -0400

    Create Install Microsoft Powershell Modules.ps1
    Just a script intended to install the modules for AzureAD, MSOnline, MicrosoftTeams, Microsoft.ONline.SharePoint.Powershell, SharePointPnPPowerShellOnline and then to update help.

---
+++ Install Microsoft Powershell Modules.ps1
@@ -0,0 +1,1 @@
+Install-Module AzureAD ; Install-Module MSOnline ; Install-Module MicrosoftTeams | Install-Module Microsoft.Online.SharePoint.Powershell ; Install-Module SharePointPnPPowerShellOnline ; Update-help
