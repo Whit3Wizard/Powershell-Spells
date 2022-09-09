61976da250665d00b2bfe954346aaaf56590adec
Whit3Wizard <112817193+Whit3Wizard@users.noreply.github.com>
2022-09-09 05:36:24 -0400

    Update Install Microsoft Powershell Modules.ps1

--- Install Microsoft Powershell Modules.ps1
+++ Install Microsoft Powershell Modules.ps1
@@ -1,1 +1,8 @@
-Install-Module AzureAD ; Install-Module MSOnline ; Install-Module MicrosoftTeams | Install-Module Microsoft.Online.SharePoint.Powershell ; Install-Module SharePointPnPPowerShellOnline ; Update-help
+Set-ExecutionPolicy -RemoteSigned
+	Install-Module AzureAD ; 
+	Install-Module MSOnline ; 
+	Install-Module MicrosoftTeams ; 
+	Install-Module Microsoft.Online.SharePoint.Powershell ; 
+	Install-Module SharePointPnPPowerShellOnline ; 
+	Install-Module PSWindowsUpdate ; 
+Update-help	
